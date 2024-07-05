52b6e3e3491d4d48f8ddc110536d49df12d7070a
Abdulaziz360 <abdulazizcw360@gmail.com>
2024-07-05 15:27:30 +0500

    react router

--- react-Router/src/components/Contact/Contact.jsx
+++ react-Router/src/components/Contact/Contact.jsx
@@ -1,4 +1,7 @@
+import { useParams } from "react-router-dom";
+
 export default function Contact() {
+    const {id}=useParams()
     return (
         <div className="relative flex items-top justify-center min-h-[700px] bg-white sm:items-center sm:pt-0">
             <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
@@ -6,7 +9,7 @@ export default function Contact() {
                     <div className="grid grid-cols-1 md:grid-cols-2">
                         <div className="p-6 mr-2 bg-gray-100 sm:rounded-lg">
                             <h1 className="text-3xl sm:text-4xl text-gray-800 font-extrabold tracking-tight">
-                                Get in touch: 
+                                Get in touch:{id} 
                             </h1>
                             <p className="text-normal text-lg sm:text-xl font-medium text-gray-600 mt-2">
                                 Fill in the form to start a conversation
--- react-Router/src/main.jsx
+++ react-Router/src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { createElement } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import { createBrowserRouter,createRoutesFromElements,Route,RouterProvider } from "react-router-dom";
+// const router=createBrowserRouter([{
+//   path:'/',
+//   element:<App/>,
+//   children:[{
+//      path:'/',
+//      element:<Home/>
+     
+//   },
+//   {
+//     path:'/about',
+//     element:<About/>
+//   },
+//   {
+//     path:'/contact',
+//     element:<Contact/>
+//   }
+// ]
+// }])
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+              <Route path='' element={<Home/>}/>
+              <Route path='about' element={<About/>}/>
+              <Route path='contact/:id' element={<Contact/>}/>
+    </Route>
+  )
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    <RouterProvider router={router}/>
+
 )
